2c26dbff2662adcd4ee68430385f020addefb29d
Igor Nazarov <grizley.tery@gmail.com>
2020-10-06 10:00:16 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = (str, bracketsConfig) => {
+    let len = bracketsConfig.length;
+    let comparator = "";
+    let count = 0;
+    let brackets = "";
+    while (len !== count) {
+        count = 0;
+        for (let i = 0; i < len; i += 1) {
+            const [first, second] = bracketsConfig[i];
+            brackets = first + second;
+            comparator = str.replace(brackets, "");
+            if (comparator === str) {
+                let number = count += 1;
+            }
+            str = comparator;
+        }
+    }
+    let result = str.length === 0;
+    return result;
 }
